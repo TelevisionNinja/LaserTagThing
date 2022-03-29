1a4fc4cda373a47458c2ca105cc2adcc0667e974
TelevisionNinja <TelevisionNinja@users.noreply.github.com>
2022-03-29 15:26:17 -0500

    udp transmit code

---
+++ udpServer.py
@@ -0,0 +1,24 @@
+import socket
+
+#------------------------------------------
+# transmit if id player that was hit
+
+broadcaster = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+def broadcast(target_id):
+    broadcaster.sendto(str(target_id).encode(), ("127.0.0.1", 7500))
+
+#------------------------------------------
+# listen to hits
+
+sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+sock.bind(("127.0.0.1", 7501))
+
+while True:
+    data = sock.recvfrom(6)
+    data_str = data[0].decode("latin-1")
+    hit_data = data_str.split(":")
+    shooter_id = int(hit_data[0]) # person who shoots
+    target_id = int(hit_data[1]) # person who gets hit
+
+    broadcast(target_id)
